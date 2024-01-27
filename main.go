5d4c6dc244024a342c17b8c655625ed423968af5
Zack Sargent <zack@zack.fyi>
2024-01-27 17:12:22 -0500

    add basic redis storage

--- main.go
+++ main.go
@@ -1,31 +1,66 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
+	"log/slog"
+	"os"
 
-	"github.com/tmc/langchaingo/llms"
-	"github.com/tmc/langchaingo/llms/ollama"
+	"github.com/nkcyber/ai-hacking-lab/services"
 	"github.com/tmc/langchaingo/schema"
 )
 
 func main() {
-	llm, err := ollama.NewChat(ollama.WithLLMOptions(ollama.WithModel("orca-mini")))
+	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	chat, err := services.NewChat("orca-mini", log)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
 	}
-	ctx := context.Background()
-	completion, err := llm.Call(ctx, []schema.ChatMessage{
-		schema.SystemChatMessage{Content: "You are a hyper-creative rhyming machine."},
-		schema.HumanChatMessage{Content: "Tell me a poem about Sam Altman"},
-	}, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-		fmt.Print(string(chunk))
-		return nil
-	}),
-	)
+	err = chat.ClearAllMessages()
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
 	}
-	_ = completion
+	chatId := services.ChatIdType("chat_1")
+	chat.AddMessage(chatId, schema.SystemChatMessage{Content: "Hello There, bruh!"})
+
+	history, err := chat.GetMessages(chatId)
+	if err != nil {
+		log.Error(err.Error())
+	} else {
+		for i, message := range history {
+			log.Info(fmt.Sprintf("message %d: %s", i, message.GetContent()))
+		}
+	}
+	// log.Info("running Llm.Call")
+	// completion, err := chat.Llm.Call(chat.Ctx, []schema.ChatMessage{
+	// 	schema.SystemChatMessage{Content: "It's very important to keep your responses as short as possible. If you write more than 3 lines, very bad things will happen. Please do not write more than 3 lines of text."},
+	// 	schema.SystemChatMessage{Content: "You are a hyper-creative rhyming machine. KEEP ALL RESPONSES SHORTER THAN THREE LINES. KEEP YOUR RESPONSES SHORT. DO NOT WRITE A LOT OF TEXT"},
+	// 	schema.HumanChatMessage{Content: "Tell me a poem about Trees"},
+	// }, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+	// 	fmt.Print(string(chunk))
+	// 	return nil
+	// }),
+	// )
+	// if err != nil {
+	// 	log.Error(err.Error())
+	// }
+	// _ = completion
+
+	// ORIGINAL
+	// llm, err := ollama.NewChat(ollama.WithLLMOptions(ollama.WithModel("orca-mini")))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// ctx := context.Background()
+	// completion, err := llm.Call(ctx, []schema.ChatMessage{
+	// 	schema.SystemChatMessage{Content: "You are a hyper-creative rhyming machine."},
+	// 	schema.HumanChatMessage{Content: "Tell me a poem about Sam Altman"},
+	// }, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+	// 	fmt.Print(string(chunk))
+	// 	return nil
+	// }),
+	// )
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// _ = completion
 }
--- services/chat.go
+++ services/chat.go
@@ -2,21 +2,52 @@ package services
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log/slog"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/tmc/langchaingo/llms/ollama"
+	"github.com/tmc/langchaingo/schema"
 )
 
+type ChatIdType string
+
 type ChatService struct {
-	ctx context.Context
-	rdb *redis.Client
-	llm *ollama.Chat
+	Ctx context.Context
+	Llm *ollama.Chat
+	log *slog.Logger
+	// There's community support for Redis chat history for TypeScript, but not Go.
+	// So, we have to roll our own, for the moment.
+	// TypeScript suppport:
+	//   <https://api.js.langchain.com/classes/langchain_community_stores_message_ioredis.RedisChatMessageHistory.html>
+	// We just store the array of messages in Redis according to the chatId.
+	// It's not a fancy solution, but this is not a fancy app.
+	rdb     *redis.Client
+	chatTTL time.Duration
+	temp    float64
 }
 
-func NewChatService(model_name string) (ChatService, error) {
-	// initalize ollama connection
-	llm, err := ollama.NewChat(ollama.WithLLMOptions(ollama.WithModel(model_name)))
+type SerializableMessage struct {
+	Content string                 `json:"content"`
+	Type    schema.ChatMessageType `json:"type"`
+}
+
+// implement schema.ChatMessage interface
+func (sm SerializableMessage) GetContent() string {
+	return sm.Content
+}
+
+// implement schema.ChatMessage interface
+func (sm SerializableMessage) GetType() schema.ChatMessageType {
+	return sm.Type
+}
+
+// initalizes ollama & redis
+func NewChat(model_name string, log *slog.Logger) (ChatService, error) {
+	llm, err := ollama.NewChat(ollama.WithLLMOptions(
+		ollama.WithModel(model_name), ollama.WithPredictPenalizeNewline(true)))
 	if err != nil {
 		return ChatService{}, fmt.Errorf("initalizing chat: %w", err)
 	}
@@ -27,11 +58,83 @@ func NewChatService(model_name string) (ChatService, error) {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+	log.Info("creating chat sevice")
 	return ChatService{
-		ctx: ctx,
-		rdb: rdb,
-		llm: llm,
+		Ctx:     ctx,
+		Llm:     llm,
+		rdb:     rdb,
+		chatTTL: 10 * time.Minute,
+		temp:    1,
+		log:     log,
 	}, nil
+}
+
+// wipes redis instance
+func (c *ChatService) ClearAllMessages() error {
+	return c.rdb.FlushDB(c.Ctx).Err()
+}
+
+// gets our serializable message format from the redis instance
+func (c *ChatService) GetSerialzableMessages(chatId ChatIdType) ([]SerializableMessage, error) {
+	// check if key exists
+	exists, err := c.rdb.Exists(c.Ctx, string(chatId)).Result()
+	if err != nil {
+		return nil, fmt.Errorf("checking if '%s' exists: %w", chatId, err)
+	}
+	// return an empty list if nothing exists
+	if exists == 0 {
+		return []SerializableMessage{}, nil
+	}
+	// get the key, assuming it exists
+	redisHistory, err := c.rdb.Get(c.Ctx, string(chatId)).Result()
+	if err != nil {
+		return nil, fmt.Errorf("getting '%s' from redis: %w", chatId, err)
+	}
+	// unmarshal redis contents
+	var history []SerializableMessage
+	err = json.Unmarshal([]byte(redisHistory), &history)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshalling history for %s: %w", chatId, err)
+	}
+	return history, nil
+}
+
+// gets the official message format from the redis instance
+func (c *ChatService) GetMessages(chatId ChatIdType) ([]schema.ChatMessage, error) {
+	history, err := c.GetSerialzableMessages(chatId)
+	if err != nil {
+		return nil, err
+	}
+	// convert from slice of structs to slice of interfaces
+	chatMessages := make([]schema.ChatMessage, len(history))
+	for i, m := range history {
+		chatMessages[i] = m
+	}
+	return chatMessages, nil
+}
+
+// adds a message to the Redis instance
+func (c *ChatService) AddMessage(chatId ChatIdType, message schema.ChatMessage) error {
+	c.log.Info("adding message to " + string(chatId))
+	history, err := c.GetSerialzableMessages(chatId)
+	if err != nil {
+		return fmt.Errorf("adding message: %w", err)
+	}
+
+	history = append(history, SerializableMessage{
+		Content: message.GetContent(),
+		Type:    message.GetType(),
+	})
+
+	historyStr, err := json.Marshal(history)
+	if err != nil {
+		return fmt.Errorf("marshalling history: %w", err)
+	}
+	err = c.rdb.Set(c.Ctx, string(chatId), historyStr, c.chatTTL).Err()
+	if err != nil {
+		return fmt.Errorf("setting key (%s) in redis: %w", chatId, err)
+	}
+	return nil
 }
 
 // TODO: load prompts
