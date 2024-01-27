25992c391ef178900d1524f435bb27816c8bccba
Zack Sargent <zack@zack.fyi>
2024-01-27 14:57:16 -0500

    add ollama example

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "context"
+    "fmt"
+    "log"
+
+    "github.com/tmc/langchaingo/llms"
+    "github.com/tmc/langchaingo/llms/ollama"
+)
 
 func main() {
-	fmt.Println("this was printed by AI!!")
+    llm, err := ollama.New(ollama.WithModel("orca-mini"))
+    if err != nil {
+        log.Fatal(err)
+    }
+    ctx := context.Background()
+    completion, err := llm.Call(ctx, "Human: Who was the first man to walk on the moon?\nAssistant:",
+        llms.WithTemperature(0.8),
+        llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+            fmt.Print(string(chunk))
+            return nil
+        }),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    _ = completion
 }
